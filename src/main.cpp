e0f48859cfced05ff65772c5e79837957cf42b07
Jimmy Xin <jxin31415@Jimmys-MBP.attlocal.net>
2021-01-15 17:46:04 -0600

    Set-up

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       jxin31415                                                 */
+/*    Created:      Fri Jan 15 2021                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}

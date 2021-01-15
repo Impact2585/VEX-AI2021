fb76c93b4b1219eafcd4bb4166b28427827db765
RGTrio <ririyennu@gmail.com>
2021-01-15 17:50:41 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,18 @@
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/*    Module:       main.cpp                                                  */
-/*    Author:       jxin31415                                                 */
-/*    Created:      Fri Jan 15 2021                                           */
-/*    Description:  V5 project                                                */
-/*                                                                            */
-/*----------------------------------------------------------------------------*/
-
 // ---- START VEXCODE CONFIGURED DEVICES ----
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
+#include "Indexer.h"
 
 using namespace vex;
-
+Indexer index();
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
-  
+  while(true) {
+    double speed = 0; // where do you get the speed from?
+    index().runForwards(speed);
+    index().runBackwards(speed);
+    Brain.sleep(50);
+  }
 }

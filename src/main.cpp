676c0f6e08dd24cc8b91b03f626667848aa2541c
Jimmy Xin <jxin31415@Jimmys-MBP.attlocal.net>
2021-01-15 23:59:59 -0600

    VexLink Demo

--- src/main.cpp
+++ src/main.cpp
@@ -20,51 +20,17 @@ tankDrive tank();
 intake intake();
 indexer indexer();
 
-/*---------------------------------------------------------------------------*/
-/*                          Pre-Autonomous Functions                         */
-/*                                                                           */
-/*  You may want to perform some actions before the competition starts.      */
-/*  Do them in the following function.  You must return from this function   */
-/*  or the autonomous and usercontrol tasks will not be started.  This       */
-/*  function is only called once after the V5 has been powered on and        */
-/*  not every time that the robot is disabled.                               */
-/*---------------------------------------------------------------------------*/
-
 void pre_auton(void) {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
 
   // All activities that occur before the competition starts
   // Example: clearing encoders, setting servo positions, ...
+
+  return;
 }
 
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              Autonomous Task                              */
-/*                                                                           */
-/*  This task is used to control your robot during the autonomous phase of   */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
-
-void autonomous(void) {
-  // ..........................................................................
-  // Insert autonomous user code here.
-  // ..........................................................................
-}
-
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              User Control Task                            */
-/*                                                                           */
-/*  This task is used to control your robot during the user control phase of */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
-
-void usercontrol(void) {
+void run(void) {
   // User control code here, inside the loop
   while (1) {
     // This is the main execution loop for the user control program.
@@ -80,13 +46,34 @@ void usercontrol(void) {
   }
 }
 
+// Demo message sender in message_link
+int sendDemo() {
+  // wait for link
+  while( !linkA.isLinked() )
+    this_thread::sleep_for(50);
+    
+  // check for connection
+  while(1) {
+    linkA.send("demoMessage");
+    this_thread::sleep_for(500);
+  }
+  return 0;
+}
+
+// Demo message receiver in message_link
+void receiveDemo( const char *message, const char *linkname, double value ) {
+  printf("%s: was received on '%s' link\n", message, linkname );
+}
+
 //
 // Main will set up the competition functions and callbacks.
 //
 int main() {
   // Set up callbacks for autonomous and driver control periods.
-  Competition.autonomous(autonomous);
-  Competition.drivercontrol(usercontrol);
+  // What are these called for AI???
+  Competition.autonomous(run);
+  Competition.drivercontrol(run);
+  linkA.received("demoMessage", receiveDemo);
 
   // Run the pre-autonomous function.
   pre_auton();
